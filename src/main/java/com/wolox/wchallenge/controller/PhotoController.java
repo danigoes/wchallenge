ef07c54b01746448db444b8c869436d61721d7f7
danigoes23 <dagoess23@gmail.com>
2021-04-23 20:50:54 -0500

    Access photos data from api

--- src/main/java/com/wolox/wchallenge/controller/PhotoController.java
+++ src/main/java/com/wolox/wchallenge/controller/PhotoController.java
@@ -5,6 +5,13 @@
  */
 package com.wolox.wchallenge.controller;
 
+import com.wolox.wchallenge.dto.PhotoDTO;
+import com.wolox.wchallenge.service.PhotoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,6 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Daniela
  */
 @RestController
+@RequestMapping("/wchallenge/api/photos")
 public class PhotoController {
     
+    @Autowired
+    private PhotoService photoService;
+    
+    @GetMapping()
+    public ResponseEntity<List<PhotoDTO>> getPhotos() {
+        List<PhotoDTO> response = this.photoService.getPhotos();
+        return ResponseEntity.ok(response);
+    }
 }
